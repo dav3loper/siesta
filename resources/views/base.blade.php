190a9c22d8786bc8e1bae6842fe5b1e307288511
Dav3loper <drav3nreloaded@gmail.com>
2019-09-11 23:11:21 +0200

    SIESTA#21: Add csrf token
    Add csrf token to help ajax requests

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{-- Meta data --}}
     <meta http-equiv="content-language" content="es">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- End Meta data --}}
     @section('styles')
