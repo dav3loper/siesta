e0dbf16a3cf743dbfcd7b28e2ea2d02c58069189
dav3loper <davekrelak@hotmail.com>
2021-09-09 20:21:06 +0200

    Fix: Add heroku incremental

--- app/Presentation/FilmFestivalListDecorator.php
+++ app/Presentation/FilmFestivalListDecorator.php
@@ -50,7 +50,7 @@ class FilmFestivalListDecorator implements \Iterator
         $lastVotedFilm = 1;
         $currentIdFilmFestival = $this->_current->getId();
         if (array_key_exists($currentIdFilmFestival, $this->_lastVotedFilmPerFestival)) {
-            $lastVotedFilm = $this->_lastVotedFilmPerFestival[$currentIdFilmFestival] + 1;
+            $lastVotedFilm = $this->_lastVotedFilmPerFestival[$currentIdFilmFestival] + 10;
         }
 
         return UrlGenerator::getShowMovie($lastVotedFilm);
