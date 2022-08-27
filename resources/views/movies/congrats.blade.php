95fed611cc9386683bdd880962848ed66bc94ebe
dav3loper <davekrelak@hotmail.com>
2022-08-27 12:58:05 +0200

    adaptar mensaje
    adaptar mensaje de la vista por si es el otro caso

--- resources/views/movies/congrats.blade.php
+++ resources/views/movies/congrats.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row margin-top-60">
             <div class="col-md-12">
-                <h1>¡Enhorabuena! Has concluido de hatear!</h1>
+                <h1>¡Enhorabuena! Has concluido de hatear o no hay nada todavia..!</h1>
                 <h3>Ya puedes dedicar tu tiempo a otros menesteres </h3>
                 <div class="margin-top-30">
                   <img src="https://media.giphy.com/media/TiUYdWptTqn8yOk1Dp/giphy.gif"/>
